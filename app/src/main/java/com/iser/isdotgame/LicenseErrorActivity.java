d270e39d0a2fb9680530cca586da4e9ad4c92fd6
poldasht <me.register@outlook.com>
2019-12-06 00:52:03 +0330

    Added sound effects

---
+++ app/src/main/java/com/iser/isdotgame/LicenseErrorActivity.java
@@ -0,0 +1,14 @@
+package com.iser.isdotgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LicenseErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license_error);
+    }
+}
